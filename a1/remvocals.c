c485c8c0700ce1c5a1b158ff7e92cb5d34d3fbb5
g4cosmic <g4cosmic@d09977a1-dd8b-42c5-bd8d-1506c6cc43db>
2015-01-26 05:07:09 +0000

    Added final (I hope) copy of part 1.

---
+++ a1/remvocals.c
@@ -0,0 +1,47 @@
+#include <stdlib.h>
+#include <stdio.h>
+#include <unistd.h>
+
+int main(int argc, char *argv[]) {
+	FILE *sourcewav, *destwav;
+
+	if (argc != 3) {
+		fprintf(stderr, "insufficient arguments\n");
+		exit(EXIT_FAILURE);
+	}
+
+	if ((sourcewav = fopen(argv[1], "rb")) == NULL) {
+		fprintf(stderr, "Can't open %s\n", argv[1]);
+		exit(EXIT_FAILURE);
+	}
+
+	if ((destwav = fopen(argv[2], "wb")) == NULL) {
+		fprintf(stderr, "Can't open %s\n", argv[2]);
+		fclose(sourcewav);
+		exit(EXIT_FAILURE);
+	}
+
+	//reading and coping the first 44 bytes
+	unsigned char buffer[44];
+	fread(&buffer, 1, 44, sourcewav);
+	fwrite(&buffer, 1, 44, destwav);
+
+	//applying the algorithm to the rest of the file
+	while (1) {
+		short left;
+		if (fread(&left, sizeof(short), 1, sourcewav) != 1) {
+			break;
+		}
+		short right;
+		if (fread(&right, sizeof(short), 1, sourcewav) != 1) {
+			break;
+		}
+		short combined = (left - right) / 2;
+		fwrite(&combined, sizeof(combined), 1, destwav);
+		fwrite(&combined, sizeof(combined), 1, destwav);
+	}
+
+	fclose(sourcewav);
+	fclose(destwav);
+	return 0;
+}
